9795eb966198c68c6abe0ca726fabc583ee9d69b
Rafał Kupka <kupson@kupson.net>
2012-05-12 01:23:24 +0200

    One stage version

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,22 +16,15 @@ class hosts {
             tag          => "for-env-${::environment}";
     }
 
-    if $::hosts_managed == $hosts::params::tmpl_version {
-        
-        Host <<| tag == "for-env-${::environment}" |>> {
-           ensure => present,
-        }
-
+    Host <<| tag == "for-env-${::environment}" |>> {
+        ensure  => present,
+        require => File['/etc/hosts'],
     }
-    else {
+
+    if $::hosts_managed != $hosts::params::tmpl_version {
         
         File['/etc/hosts'] {
             content => template('hosts/hosts.erb'),
-        }
-
-        notify {
-            'hosts_replaced':
-                message => 'File /etc/hosts replaced with new template. Please run puppet again to add missing entries.';
         }
 
     }
