b1c9504e2551c31424edb3feb0f417257e253017
Rafał Kupka <kupson@kupson.net>
2012-05-12 01:07:47 +0200

    Working hosts class
    Two stage design.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,38 @@
 class hosts {
+    include hosts::params
+
+    file {
+        '/etc/hosts':
+            ensure => file,
+            owner  => 'root',
+            group  => 'root',
+            mode   => 0644;
+    }
+
+    @@host {
+        $::fqdn:
+            ip           => $::ipaddress,
+            host_aliases => [ $::hostname ],
+            tag          => "for-env-${::environment}";
+    }
+
+    if $::hosts_managed == $hosts::params::tmpl_version {
+        
+        Host <<| tag == "for-env-${::environment}" |>> {
+           ensure => present,
+        }
+
+    }
+    else {
+        
+        File['/etc/hosts'] {
+            content => template('hosts/hosts.erb'),
+        }
+
+        notify {
+            'hosts_replaced':
+                message => 'File /etc/hosts replaced with new template. Please run puppet again to add missing entries.';
+        }
+
+    }
 }
---
+++ manifests/params.pp
@@ -0,0 +1,6 @@
+class hosts::params {
+
+    $tmpl_version = '0.1'
+    $puppet_ip = extlookup('puppet_ip')
+
+}
